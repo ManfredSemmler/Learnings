e422eab3a9aea5f1fd364f261cfb6c1939762854
Semmler, Manfred (MSE) <manfred.semmler@eos.info>
2024-03-01 14:08:52 +0100

    two variants of using lambda

--- ConsoleApplication_lambda.cpp
+++ ConsoleApplication_lambda.cpp
@@ -12,15 +12,58 @@ void ForEach(const std::vector<int>& values, const std::function<void(int)>& fun
         func(value);
 }
 
+
+int mySort()
+{
+    std::vector<int> mylist = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5 };
+
+    // Drucken mit Lambda als definierte funktion, die dann als Parameter in der Funktion verwendet wird
+    auto print_X = [](int value) {std::cout << std::endl << value << ", "; };
+
+    std::cout << std::endl << "unsorted" << std::endl;
+    for_each(mylist.begin(), mylist.end(), print_X);
+
+    //sort
+    std:sort(mylist.begin(), mylist.end(), std::greater<>());
+    //print
+    std::cout << std::endl << "sorted" << std::endl;
+    for_each(mylist.begin(), mylist.end(), print_X);
+
+    return 0;
+}
+
+
+int mySort_2()
+{
+    std::vector<int> mylist = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5 };
+
+    // Drucken mit Lambda direkt in den Funktionsparameter eingebaut
+    
+    std::cout << std::endl << "unsorted L2" << std::endl;
+    for_each(mylist.begin(), mylist.end(), [](int value) {std::cout << std::endl << value << ", "; });
+
+    //sort
+    std:sort(mylist.begin(), mylist.end(), std::greater<>());
+    //print
+    std::cout << std::endl << "sorted L2" << std::endl;
+    for_each(mylist.begin(), mylist.end(), [](int value) {std::cout << std::endl << value << ", "; });
+
+    return 0;
+}
+
+
+
 int main()
 {
  	std::vector<int> values = { 1,5,4,2,3,8};
-
-	int a = 5;
-
+    int a = 5;
     auto lambda = [=](int value)  { std::cout << "Value: " << value << std::endl; };
 
+    std::cout << "-------------- My ForEach-------------" << std::endl;
     ForEach(values, lambda);
+
+    std::cout << "-------------- My Sort-------------" << std::endl;
+    mySort_2();
     std::cin.get();
 }
 
